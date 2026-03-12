27ada7be9a38a0639b12ec9615e293e91e44d7a7
Jhonatan-Yamil <sellooricalcos@gmail.com>
2026-03-12 17:53:10 -0400

    feat: first draft of system

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import MainLayout from "./components/layout/MainLayout";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import OrdersPage from "./pages/OrdersPage";
+import PaymentsPage from "./pages/PaymentsPage";
+import LogisticsPage from "./pages/LogisticsPage";
+import ClientsPage from "./pages/ClientsPage";
+import ProductsPage from "./pages/ProductsPage";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/pedidos" element={<OrdersPage />} />
+            <Route path="/pagos" element={<PaymentsPage />} />
+            <Route path="/logistica" element={<LogisticsPage />} />
+            <Route path="/clientes" element={<ClientsPage />} />
+            <Route path="/productos" element={<ProductsPage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
