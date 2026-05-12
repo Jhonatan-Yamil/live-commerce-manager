5f4a297652c82c9dc7083e430db10cfdecd3ebeb
Jhonatan-Yamil <sellooricalcos@gmail.com>
2026-05-12 15:06:52 -0400

    fixed: nginx and docker deploy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 echo "✅ Migrations completed"
 
 echo "🚀 Starting FastAPI..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips="127.0.0.1"
